f658021baf85c0627b29eb327d049a2f47c7cf1b
fyc1007261 <fyc1007261@sjtu.edu.cn>
2018-03-22 23:39:05 +0800

    init table

--- src/App.js
+++ src/App.js
@@ -2,22 +2,56 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+let data=[
+    {Name : 'Book A', Author: 'Author A'},
+    {Name : 'Book b', Author: 'Author b'},
+    {Name : 'Book d', Author: 'Author d'}
+];
 
-class Msg extends Component {
+
+function Msg (props) {
+        return (
+            <table>
+                <tbody>
+                {props.value}
+                </tbody>
+            </table>
+        );
+}
+
+class Tbl extends Component {
+    constructor(props){
+        super();
+        this.state = {
+            a : 1,
+            tableArray : []
+        };
+        let len = props.values.length;
+        for (let i=0; i<len; i++){
+            let temp = props.values[i];
+            let tbls = this.state.tableArray;
+            tbls.push(<tr><td>{temp.Name}</td><td>{temp.Author}</td></tr>);
+            this.setState({tableArray:tbls});
+            console.log(this.state.tableArray);
+        }
+    }
+    onclick() {
+        console.log(this.state.a);
+        this.setState({a:this.state.a+1})
+    }
     render() {
         return (
-            <div>Hello {this.props.name}</div>
+            <Msg value={this.state.tableArray}/>
         );
     }
 }
 
-class App extends Component {
-  render() {
-    return (
-        <Msg name="<myname>"/>
-    );
-  }
+class App extends Component{
+    render(){
+        return(
+            <Tbl values={data}/>
+        )
+    }
 }
-
 
 export default App;
